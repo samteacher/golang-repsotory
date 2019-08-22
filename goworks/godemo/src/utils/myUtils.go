905c2b85fe73a0db830596631ace9c4533549d18
yuqiufu <1327499522@qq.com>
2019-08-22 21:35:08 +0800

    修改结构;添加笔记;新增包

---
+++ goworks/godemo/src/utils/myUtils.go
@@ -0,0 +1,33 @@
+// 本人自己封闭的一些常用的工具及方法
+
+package util
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// 如果某个函数的入参是interface{}，有下面几种方式可以获取入参的方法：
+// 1.fmt
+func Typeof(v interface{}) string {
+	return fmt.Sprintf("%T", v)
+}
+
+// 2.反射
+func Typeof2(v interface{}) string {
+	return reflect.TypeOf(v).String()
+}
+
+// 3.类型断言
+func Typeof3(v interface{}) string {
+	switch t := v.(type) {
+	case int:
+		return "int"
+	case float64:
+		return "float64"
+	//... etc
+	default:
+		_ = t
+		return "unknown"
+	}
+}
---
+++ goworks/godemo/src/utils/strx_test.go
@@ -0,0 +1,101 @@
+package util
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStr_HashCode(t *testing.T) {
+	var testStr = "abcd"
+	code := New(testStr).HashCode()
+	t.Log(code)
+}
+func TestNew_NoConsecutiveSpaces(t *testing.T) {
+	var testStr = " a   bcde   fgh      ijkl   mn"
+
+	s := New(testStr).NoConsecutiveSpaces().Val()
+	fmt.Println(s)
+}
+
+func TestIsEmpty(t *testing.T) {
+	var testStr = "   "
+	r := IsEmpty(testStr)
+	r2 := IsNotEmpty(testStr)
+	fmt.Println(r, r2)
+}
+
+func TestNew_TrimRight(t *testing.T) {
+	var testStr = "世界和平fj"
+	str := New(testStr)
+	resultStr := str.TrimRight("fj").Val()
+	t.Log(resultStr)
+}
+
+func TestNew_Between(t *testing.T) {
+	var testStr = "今天天气真好啊，太阳当空照耀。"
+	str := New(testStr)
+	resultStr := str.Between("今天", "耀").Val()
+	t.Log(resultStr)
+}
+
+func TestBetween(t *testing.T) {
+	var newStr string = New(" 这*里|||有人民yyyy币         $yy#{money}   ..").
+		NoConsecutiveSpaces().
+		Trim(" ").
+		Clean("*", "|", "yy").
+		Replace("$", "￥").
+		Replace("#{money}", "250").
+		TrimRight(".").
+		Append("元").
+		Val()
+	fmt.Println(newStr)
+}
+
+func TestStr_Clean(t *testing.T) {
+	var testStr = "abbcde ffsa*b*34"
+	str := New(testStr)
+	resultStr := str.Clean(" ", "*", "bb").Val()
+	t.Log(resultStr)
+}
+
+func TestStr_ReplaceM(t *testing.T) {
+	var testStr = "a|d*sdf"
+
+	str := New(testStr)
+	m := str.ReplaceM("rrr", "|", "*")
+	t.Log(m.Val())
+}
+
+func TestStr_Append(t *testing.T) {
+	var testStr = "asdf"
+
+	str := New(testStr)
+
+	s := str.Append(" | sdf").Append(" - fff").Val()
+	t.Log(s)
+}
+
+func TestStr_Replace(t *testing.T) {
+	var testStr = "777-${a}-${b}"
+	y := New(testStr)
+	resultStr := y.Replace("${a}", "999").Replace("${b}", "234").Val()
+	t.Log(resultStr)
+}
+
+func TestStr_Reverse(t *testing.T) {
+	y := New("中国")
+	reverse := y.Reverse().Val()
+	t.Log(reverse)
+}
+
+func TestStr_ToLower(t *testing.T) {
+	y := New("Hello,world!")
+	lower := y.ToLower().Val()
+	t.Log(lower)
+}
+
+func TestStr_ToUpper(t *testing.T) {
+	y := New("Hello,world!")
+	upper := y.ToUpper().Val()
+	t.Log(upper)
+}
